5a876ca49516820f740b7010132bdeecba5123e0
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-08-22 18:04:33 +0530

    Create problem35.py

---
+++ level2/problem35.py
@@ -0,0 +1,27 @@
+#PF-Prac-35
+def convert_to_roman(num):
+    #Start writing your code here
+    val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+    syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+    roman_num = ''
+    i = 0
+    while  num > 0:
+        for _ in range(num // val[i]):
+            roman_num += syb[i]
+            num -= val[i]
+        i += 1
+    return roman_num
+
+
+for num in range(1,5000):    
+    print(num," : ",convert_to_roman(num))
