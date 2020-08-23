ae4e410ed70285fdee89681452adecde3eba4e15
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-08-23 11:10:31 +0530

    Create problem18.py

---
+++ level3/problem18.py
@@ -0,0 +1,15 @@
+import datetime
+def date_by_adding_business_days(from_date, add_days):
+    business_days_to_add = add_days
+    current_date = from_date
+    while business_days_to_add > 0:
+        current_date += datetime.timedelta(days=1)
+        weekday = current_date.weekday()
+        if weekday >= 5: # sunday = 6
+            continue
+        business_days_to_add -= 1
+    return current_date
+
+#demo:
+print '10 business days from today:'
+print date_by_adding_business_days(datetime.date.today(), 10)
